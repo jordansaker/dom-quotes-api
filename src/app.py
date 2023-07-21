0a4698ccb8e6bd98c0e8ace03d8dcd22ad4a9a1d
jordansaker <jordan.saker@ymail.com>
2023-07-21 13:31:05 +1000

    created quote model

--- src/app.py
+++ src/app.py
@@ -12,8 +12,10 @@ def create_app():
     Function to create the Flask app
     """
     app = Flask(__name__)
-
+    # configure the app using the object from config.py
     app.config.from_object(app_config)
+    app.json.sort_keys = False
+    # initalise the instances from the init.py file
     db.init_app(app)
     ma.init_app(app)
 
---
+++ src/models/quote.py
@@ -0,0 +1,37 @@
+"""
+Quotes Model and schema
+
+The Quote Model contains the following attributes:
+
+    id, quote, movie_title
+"""
+from init import db, ma
+
+class Quote(db.Model):
+    """
+    Creates a model instance of the database instance
+
+    Attributes:
+
+        quote (text),  movie_title (str)
+    """
+    __tablename__ = 'dom_quotes'
+    # model attributes
+    id = db.Column(db.Integer, primary_key=True)
+    quote = db.Column(db.Text, unique=True, nullable=False)
+    movie_title = db.Column(db.String(), nullable=False)
+
+
+class QuoteSchema(ma.Schema):
+    """
+    Defines the schema for the Quote Model
+
+    class Meta:
+        fields = ('id', 'quote', 'movie_title')
+    """
+    class Meta:
+        """
+        Defining the fields in a Tuple
+        """
+        fields = ('id', 'quote', 'movie_title')
+        ordered = True
