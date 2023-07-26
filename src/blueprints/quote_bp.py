22c25218d9524762bf4b2711555b4a98484d3a52
jordansaker <jordan.saker@ymail.com>
2023-07-26 15:11:14 +1000

    updated code comments

--- src/blueprints/quote_bp.py
+++ src/blueprints/quote_bp.py
@@ -18,14 +18,13 @@ def get_quote():
     Get a random quote from the dom_quotes table
     """
     # get the range for randint
-    # query the database table, order by descending, get the first record
-    # get the record id and set as the max range for randint
+    # query the database table, order by descending
     stmt = db.select(Quote).order_by(desc(Quote.id))
     quotes = db.session.scalars(stmt).all()
 
-    # grab the first object in the list and access the id key
+    # set the max range index to list - 1
     max_range = len(quotes) - 1
-    # get a random number to use as the query condition
+    # get a random number to use as the index for the quotes list
     random_quote = quotes[randint(1, max_range)]
     return QuoteSchema().dump(random_quote)
 
